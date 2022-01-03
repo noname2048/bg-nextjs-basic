df416b228f8d8def09966db30d1570fc32c97067
noname2048 <sungwook.csw@gmail.com>
2022-01-03 19:23:05 +0900

    feat: feature movie api

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function NavBar() {
@@ -6,21 +7,38 @@ export default function NavBar() {
 
   return (
     <nav>
+      {/* <img src="/vercel.svg" alt="" /> */}
       <Link href="/">
         <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About us</a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const API_KEY = process.env.SECRET_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&region=KR`,
+      },
+    ];
+  },
+};
